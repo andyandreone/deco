c85cd06c6ff237c2235989654b1825974cc2a5b6
andyandreone <andy1.-@hotmail.com>
2021-06-24 22:40:26 -0300

    Sincronizar ItemCount

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,33 +3,16 @@ import '../css/ItemCount.css'
 import Button from 'react-bootstrap/Button'
 
 
-export default function ItemCount({stock, initial}) {
+export default function ItemCount(props) {
 
-    function restar(){
-      if(count>1){setCount(count -1)}
-    }
-
-    function sumar(){
-      if(count<stock){setCount(parseInt(count) +1)}
-    }
-
-    function onAdd(){
-        setAdd(count);
-    }
-
-    const [count, setCount] = React.useState(initial);
-    const [add, setAdd] = React.useState(0);
+    
     return (
-        <div>
+        
             <div className="itemCount">
-                <Button variant="secondary" onClick={restar}>-</Button>
-                <p className="cantidadItemCount">{count}</p>
-                <Button variant="secondary" onClick={sumar}>+</Button>
+                <Button variant="secondary" onClick={props.restarItem}>-</Button>
+                <p className="cantidadItemCount">{props.count}</p>
+                <Button variant="secondary" onClick={props.sumarItem}>+</Button>
             </div>
-            <div>
-                <Button variant="info" onClick={onAdd}>Agregar al carrito</Button>
-                <p>Cantidad Agregada {add}</p>
-            </div>   
-        </div>
+            
     )
 }
