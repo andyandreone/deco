ef737b57ee064afb49dc47de605626160265754e
andyandreone <andy1.-@hotmail.com>
2021-07-08 23:46:21 -0300

    Componente Orden

--- src/components/DataBuyer.js
+++ src/components/DataBuyer.js
@@ -1,88 +1,100 @@
-import React, {useState} from "react";
-import { Form, Col,Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import '../css/DataBuyer.scss';
+import "../css/DataBuyer.scss";
 import { useDataContext } from "./CartContext";
-import {getFireStore} from './firebase.js'
-import * as firebase from 'firebase';
+import { getFireStore } from "./firebase.js";
+import * as firebase from "firebase";
 import Orden from "./Orden";
 
-
-
 function DataBuyer() {
-const [email, setEmail] = useState("");
-const [name, setName] = useState("");
-const [phone, setPhone] = useState("");
-const [orderId, setOrderId] = useState("");
-const [loading, setLoading] = useState("true")
-const [mostrar, setMostrar] = useState('false')
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [orderId, setOrderId] = useState(null);
+  const [mostrar, setMostrar] = useState("false");
 
-const data = useDataContext();
-    
+  const data = useDataContext();
 
-function handleChangeEmail(e) {
-  setEmail(e.target.value);
-}
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
+  }
 
-function handleChangeName(e) {
-  setName(e.target.value);
-}
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
 
-function handleChangePhone(e) {
-  setPhone(e.target.value);
-}
+  function handleChangePhone(e) {
+    setPhone(e.target.value);
+  }
 
-function addDataFirebase(){
-  const db = getFireStore();
-  const orders = db.collection("orders");
-  const newOrder = {buyers : {
-                        correo : email,
-                        nombre : name,
-                          telefono : phone,
-                              },
-                    items: data,  
-                    }
- orders.add(newOrder).then(({id})=>{
-   
-   setOrderId(id);
- }).catch(err =>{
-   console.log(err)
- }).finally(()=>{
-  setMostrar('true');
- })
- 
- 
-}
+  function addDataFirebase() {
+    const db = getFireStore();
+    const orders = db.collection("orders");
+    const newOrder = {
+      buyers: {
+        correo: email,
+        nombre: name,
+        telefono: phone,
+      },
+      items: data,
+    };
+    orders
+      .add(newOrder)
+      .then(({ id }) => {
+        setOrderId(id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
- if(mostrar == 'false'){
-  return (
-    <Form className="formDataBuyer">
-    <Col>
-    <Form.Label>Email</Form.Label>
-      <Form.Control type="email" placeholder="name@example.com" onChange={handleChangeEmail}/>
-    </Col>
- 
-    <Col>
-    <Form.Label>Nombre</Form.Label>
-      <Form.Control type="name" placeholder="Nombre" onChange={handleChangeName}/>
-    </Col>
- 
-    <Col>
-    <Form.Label>Telefono</Form.Label>
-      <Form.Control type="phone" placeholder="Telefono" onChange={handleChangePhone}/>
-    </Col>
-    <Col>
-      <Link>
-      <Button disabled={!(email !== '' && name !== '' && phone !== '')}
-      variant="outline-primary" className="buttonfinalizarCompra" onClick={addDataFirebase}>Finalizar Compra</Button>
-    </Link> 
-  </Col>
+  if (orderId == null) {
+    return (
+      <Form className="formDataBuyer">
+        <Col>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            onChange={handleChangeEmail}
+          />
+        </Col>
 
-  </Form>
-  )}else{return (mostrar ? <Orden id={orderId}/> : "")}
-    
-  
-      
+        <Col>
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Nombre"
+            onChange={handleChangeName}
+          />
+        </Col>
+
+        <Col>
+          <Form.Label>Telefono</Form.Label>
+          <Form.Control
+            type="phone"
+            placeholder="Telefono"
+            onChange={handleChangePhone}
+          />
+        </Col>
+        <Col>
+          <Link>
+            <Button
+              disabled={!(email !== "" && name !== "" && phone !== "")}
+              variant="outline-primary"
+              className="buttonfinalizarCompra"
+              onClick={addDataFirebase}
+            >
+              Finalizar Compra
+            </Button>
+          </Link>
+        </Col>
+      </Form>
+    );
+  } else {
+    return orderId !== null ? <Orden id={orderId} /> : "";
+  }
 }
 
 export default DataBuyer;
--- src/components/Orden.js
+++ src/components/Orden.js
@@ -1,53 +1,52 @@
-import React, {useState, useEffect} from 'react'
-import {getFireStore} from './firebase.js'
-import * as firebase from 'firebase';
-import '../css/Orden.scss'
-
-
-
-
+import React, { useState, useEffect } from "react";
+import { getFireStore } from "./firebase.js";
+import * as firebase from "firebase";
+import "../css/Orden.scss";
 
 function Orden(props) {
-    const [detalleCompra, setDetalleCompra] = useState(null)
-    
-    useEffect(()=>{ 
-        const db = getFireStore()
-        const ordersCollection = db.collection("orders")
-        const compra = ordersCollection.doc(props.id)
-        compra.get().then((doc)=>{
-            if(!doc.exists){
-                console.log("No results!");
-                return;
-            }
-            setDetalleCompra({id: doc.id, ...doc.data()});
-        }).catch((error)=>{
-            console.log("Error searching items", error);
-        })
-    },[]);
+  const [detalleCompra, setDetalleCompra] = useState(null);
+  let total = 0;
+  let totalCompra = 0;
 
+  useEffect(() => {
+    const db = getFireStore();
+    const ordersCollection = db.collection("orders");
+    const compra = ordersCollection.doc(props.id);
+    compra
+      .get()
+      .then((doc) => {
+        if (!doc.exists) {
+          console.log("No results!");
+          return;
+        }
+        setDetalleCompra({ id: doc.id, ...doc.data() });
+      })
+      .catch((error) => {
+        console.log("Error searching items", error);
+      });
+  }, []);
 
-    if(detalleCompra!==null){
-        return (
-            <div className="detalleCompra">
-                <p>Codigo de compra: {props.id}</p>
-                <p>Nombre: {detalleCompra.buyers.nombre}</p>
-                <p>Correo: {detalleCompra.buyers.correo}</p>
-                <p>Telefono: {detalleCompra.buyers.telefono}</p>
-                {detalleCompra.items.map((producto, i)=>{
-                    return(<ul key={i}>
-                             <li>{producto.titulo}</li>
-                             <li>{producto.descripcion}</li>
-                             <li>{producto.cantidad}</li>
-                    </ul>
-                   
-                    )
-                })}
-            </div>
-        )
-    }else{
-        return (<p>Cargando</p>)
-    }
-   
+  if (detalleCompra !== null) {
+    return (
+      <div className="detalleCompra">
+        <p>Codigo de compra: {props.id}</p>
+        <p>Nombre: {detalleCompra.buyers.nombre}</p>
+        <p>Correo: {detalleCompra.buyers.correo}</p>
+        <p>Telefono: {detalleCompra.buyers.telefono}</p>
+        {detalleCompra.items.map((producto, i) => {
+          return (
+            <ul key={i}>
+              <li>{producto.titulo}</li>
+              <li>{producto.descripcion}</li>
+              <li>{producto.cantidad}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return <p>Cargando</p>;
+  }
 }
 
-export default Orden
+export default Orden;
